972a5eda25da5ee6d1525c46b4185d7082f86640
rgsk <guptarahul70322@gmail.com>
2023-08-08 23:43:12 +0530

    Initial Commit

---
+++ Skartner/SkartnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkartnerApp.swift
+//  Skartner
+//
+//  Created by Rahul Gupta on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SkartnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
